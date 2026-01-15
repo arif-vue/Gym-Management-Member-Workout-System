7b387679d7897bdbdd613b80bd641a1d21e8cca6
“arif-vue” <arif.nuxt@gmail.com>
2026-01-15 22:20:47 +0600

    ready for deployment

--- gym_management/settings.py
+++ gym_management/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import dj_database_url
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -84,13 +87,19 @@ WSGI_APPLICATION = 'gym_management.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {
+    'default': dj_database_url.config(
+        default='postgresql://gym_management_db_dibb_user:V7hbhDghgd8ZcrCJa4rVXawjKy2rxSz5@dpg-d5kh1k6r433s73erdu70-a.oregon-postgres.render.com/gym_management_db_dibb',
+        conn_max_age=600
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
